4115dd033a88e9b575916c93f83fdb11d03735a6
Charles042 <ekechukwuma20@gmail.com>
2024-01-26 13:58:48 +0100

    Added Median

--- script.js
+++ script.js
@@ -11,9 +11,21 @@ class statistics {
         const sum = this.number.reduce((value, val)=> value + val,0);
         return sum/ this.number.length;
     }
+//median
+    forMedian(){
+        const sortedValue = this.number.slice().sort((a,b)=> a - b);
+        const midIndex = Math.floor(sortedValue.length/2);
+
+        if (sortedValue % 2 === 0){
+            return (sortedValue[midIndex - 1] + sortedValue[midIndex])/ 2;
+        }else{
+            return sortedValue[midIndex];
+        }
+    }
 }
 const number = [1,2,3,4,5,6,6];
 
 const stats = new statistics(number);
 
-console.log('mean:', mean.forMean());
+console.log('mean:', stats.forMean());
+onsole.log('median:', stats.forMedian());
